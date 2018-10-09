1de33c92f37166aebecdfcebc775e2a4c4d7c070
ROBSON PEREIRA SILVA <robson.pereira@viavarejo.com.br>
2018-10-09 19:24:20 -0300

    Adição de consulta

--- src/main/java/br/com/sherwin/controller/PostController.java
+++ src/main/java/br/com/sherwin/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +24,16 @@ public class PostController {
 
         ModelAndView mv = new ModelAndView("/post");
         mv.addObject("posts", service.findAll());
+
+        return mv;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id) {
+
+        ModelAndView mv = new ModelAndView("/post");
+        Post post = service.findOne(id).get();
+        mv.addObject("posts", post);
 
         return mv;
     }
